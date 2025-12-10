a1095545f1a50cc8564b5174777543723631f010
Premnath <vijayanand.premnath@worldline.com>
2025-12-10 19:55:37 +0530

    key bord input

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Store command output in variable
-current_date=$(date)
-echo "Today is: $current_date"
-
-user_count=$(who | wc -l)
-echo "Number of users logged in: $user_count"
+echo "What is your name?"
+read name
+echo "Hello, $name!"
