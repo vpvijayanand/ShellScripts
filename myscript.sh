778cd53105fbbab3608ee8dc06e5b938c9260f37
Vijayanand Premnath <vpvijayanand@gmail.com>
2025-12-10 20:01:42 +0530

    Fix indentation and add newline at end of file

--- myscript.sh
+++ myscript.sh
@@ -6,3 +6,12 @@ if [ $age -ge 18 ]
 then
     echo "You are an adult"
 fi
+
+read -p "Enter your age: " age
+
+if [ $age -ge 18 ]
+then
+    echo "You can vote"
+else
+    echo "You cannot vote yet"
+fi
