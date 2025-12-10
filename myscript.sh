fb58684fcd9a2abcd4cbefc68b318bc80a6bb9c6
Premnath <vijayanand.premnath@worldline.com>
2025-12-10 21:03:05 +0530

    menu

--- myscript.sh
+++ myscript.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-target=$((RANDOM % 100 + 1))
-attempts=0
-
-echo "Guess a number between 1 and 100"
-
 while true
 do
-    read -p "Your guess: " guess
-    attempts=$((attempts + 1))
+    echo ""
+    echo "=== System Menu ==="
+    echo "1. Show date and time"
+    echo "2. Show current directory"
+    echo "3. List files"
+    echo "4. Show disk usage"
+    echo "5. Exit"
+    echo ""
     
-    if [ $guess -eq $target ]
-    then
-        echo "Correct! You won in $attempts attempts"
-        break
-    elif [ $guess -lt $target ]
-    then
-        echo "Too low!"
-    else
-        echo "Too high!"
-    fi
+    read -p "Enter choice [1-5]: " choice
+    
+    case $choice in
+        1)
+            date
+            ;;
+        2)
+            pwd
+            ;;
+        3)
+            ls -l
+            ;;
+        4)
+            df -h
+            ;;
+        5)
+            echo "Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
 done
