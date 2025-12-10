5961fbcde0d234a953e8ccaee19695bbadb09f36
Premnath <vijayanand.premnath@worldline.com>
2025-12-10 20:02:53 +0530

    ifelif

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# if conditional example
-age=18
 
-if [ $age -ge 18 ]
+read -p "Enter your marks: " marks
+
+if [ $marks -ge 90 ]
 then
-    echo "You are an adult"
+    echo "Grade: A"
+elif [ $marks -ge 75 ]
+then
+    echo "Grade: B"
+elif [ $marks -ge 60 ]
+then
+    echo "Grade: C"
+else
+    echo "Grade: F"
 fi
