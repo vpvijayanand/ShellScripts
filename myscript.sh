5cc66bedf9ce660bd437f4ed690de3a883ac54e6
Premnath <vijayanand.premnath@worldline.com>
2025-12-10 19:51:15 +0530

    Special Variables

--- myscript.sh
+++ myscript.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Variable declaration (no spaces around =)
-name="John"
-age=25
-city="Chennai"
-
-# Using variables (use $ to access value)
-echo "Name: $name"
-echo "Age: $age"
-echo "City: $city"
-
-# Alternative syntax with curly braces (safer)
-echo "Hello, ${name}!"
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "All arguments: $@"
+echo "Number of arguments: $#"
