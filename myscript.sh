2c88be3b066bed5f7031c8785004135016cb6c1c
Premnath <vijayanand.premnath@worldline.com>
2025-12-10 20:55:32 +0530

    loop

--- myscript.sh
+++ myscript.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
-file="test.txt"
-
-if [ -f "$file" ]
-then
-    echo "File exists"
-else
-    echo "File does not exist"
-fi
-
-#Common file tests:
-
-#-f : file exists and is regular file
-#-d : directory exists
-#-r : file is readable
-#-w : file is writable
-#-x : file is executable
+# loops
+for name in Alice Bob Charlie David
+do
+    echo "Hello, $name"
+done
